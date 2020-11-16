72a29bdf27c7c7d9f87da223ec200f4b284498b4
FrankThomasTveter <f.t.tveter@gmail.com>
2020-11-16 13:31:44 +0100

    Added options to target fill-colors.

--- src/svg/extract.pl
+++ src/svg/extract.pl
@@ -5,16 +5,36 @@
 # Extract svg-icons for use in chart
 #
 use strict;
-                            
 
 sub Usage {                      
     print STDERR "
-Usage: extract.pl files
+Usage: extract.pl -bg \"#fff\" -bg \"#C60000\" -fg \"#000\" [files]
+
+Options:
+    -fg <color>  Color is substituted by foreground color variable.
+    -bg <color>  Color is substituded by background color variable.
+    -list        List colors in files
 
 extract takes svg-files and creates defaults.json input...
 \n";                             
     exit 1;                      
-}                                
+}
+
+my @fgs=();
+my @bgs=();
+my @colorsB=();
+my @colorsA=();
+my $list=0;
+                            
+## Look for options in argument list
+while ($_ = $ARGV[0],/^-./) {
+    shift;
+    if (/^-fg$/) {push(@fgs, shift @ARGV); next;}
+    if (/^-bg$/) {push(@bgs, shift @ARGV); next;}
+    if (/^-list$/) {$list=1; next;}
+    &Usage;
+}
+if (! @ARGV) {&Usage;}
 #
 my @files = @ARGV;
 for my $file (@files) {
@@ -25,7 +45,7 @@ for my $file (@files) {
 	while (my $line=<FH>){
 	    $str=$str . $line;
 	};
-	close(FH);
+	close(FH);	
 	$str =~ s/\n/ /g;
 	$str =~ s/<\?xml[^>]*>//g;
 	#$str =~ s/<defs[^\/>]*\/>//g;
@@ -37,13 +57,65 @@ for my $file (@files) {
 	$str =~ s/width=\"[^\"]*\"/width=\"Size\"/g;
 	$str =~ s/color:\#[^;]*;/color:fg;/g;
 	$str =~ s/stroke:\#[^;]*;/stroke:fg;/g;
-	$str =~ s/fill:\#[^;]*;/fill:bg;/g;
+	while ($str =~ /fill:(\#[^;]*);/g) {
+	    push (@colorsB, $1);
+	}
+	while ($str =~ /fill="(\#[^"]*)"/g) {
+	    push (@colorsB, $1);
+	}
+	if (! @fgs && ! @bgs) {
+	    $str =~ s/fill:\#[^;]*;/fill:bg;/g;
+	} else {
+	    foreach my $sfg (@fgs) {
+		$str =~ s/fill:$sfg;/fill:fg;/g;
+		$str =~ s/fill="$sfg"/fill="fg"/g;
+	    };
+	    foreach my $sbg (@bgs) {
+		$str =~ s/fill:$sbg;/fill:bg;/g;
+		$str =~ s/fill="$sbg"/fill="bg"/g;
+	    };
+	};
+	while ($str =~ /fill:(\#[^;]*);/g) {
+	    push (@colorsA, $1);
+	}
+	while ($str =~ /fill="(\#[^"]*)"/g) {
+		push (@colorsA, $1);
+	};
 	$str =~ s/>\s*/>/g;
 	$str =~ s/\s*</</g;
 	$str =~ s/\"/\\\"/g;
-	
-	print ("\"$value\":\"$str\",\n");
+	if (!$list) {print ("\"$value\":\"$str\",\n");};
 	
     }
-
 }
+sub uniq {
+    my %seen;
+    return grep { !$seen{$_}++ } @_;
+}
+if ($list) {
+    my $cmd="$0";
+    if (@colorsB) {
+	foreach my $col (uniq(@colorsB)) {
+	    $cmd = $cmd . " -bg \"$col\"";
+	}
+    };
+    for my $file (@files) {
+	$cmd = $cmd . " $file";
+    }
+    print "$cmd\n";
+} else {
+    if (@colorsB) {
+	print "Colors before:";
+	foreach my $col (uniq(@colorsB)) {
+	    print " \"" . $col. "\"";
+	}
+	print "\n";
+    }
+    if (@colorsA) {
+	print "Colors after: ";
+	foreach my $col (uniq(@colorsA)) {
+	    print " \"" . $col. "\"";
+	}
+	print "\n";
+    };
+};
