b818e82382897113b0caf658a2819cb7a507e9d6
FrankThomasTveter <f.t.tveter@gmail.com>
2021-09-30 11:56:45 +0200

    Removed folder from repository

--- src_new/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from './react/AppComponent';
-
-ReactDOM.render(<AppComponent />, document.getElementById('root'));
-
